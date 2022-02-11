7673b9107e1e6be17e0e7c4d6715b793550ef0f8
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-11 11:05:17 +0900

    Basic validations

--- app/models/player_input.rb
+++ app/models/player_input.rb
@@ -1,4 +1,6 @@
 class PlayerInput < ApplicationRecord
   belongs_to :player
   belongs_to :lobby
+  validates :input_type, presence: true, inclusion: { in: %w[string boolean integer] } # can be modified later
+  validates :input_value, presence: true
 end
