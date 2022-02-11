92f9ac10338fc39e2305012a30a118c80ece44ee
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-11 21:31:34 +0900

    Basic seeds

---
+++ db/games_data_seeds.rb
@@ -0,0 +1,16 @@
+class CreateGamesDataSeeds
+
+  def self.generate_sentences
+    [
+      "Never have I ever pretended to know someone I didn't.",
+      "Never have I ever watched Star Wars.",
+      "Never have I ever created a Youtube video.",
+      "Never have I ever colored my hair a crazy color.",
+      "Never have I ever caught something on fire while cooking.",
+      "Never have I ever drawn on someone when they were asleep.",
+      "Never have I ever tried parachuting.",
+      "Never have I ever had to wear braces."
+    ]
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Require other files
+require_relative 'users_seeds'
+require_relative 'games_data_seeds'
+
+# Destroy previous seeds
+puts "Deleting previous seeds..."
+
+User.destroy_all
+
+puts "Previous seeds have been deleted!"
+
+
+# Create Users seeds
+puts "Creating users..."
+
+CreateUsersSeeds.generate_users
+
+puts "Users have been created!"
+
+
+# Create Game
+puts "Creating the game..."
+
+game = Game.create!(name: "Never Have I Ever")
+
+puts "Game created!"
+
+# Create Game Data
+puts "Creating game data..."
+
+sentences = CreateGamesDataSeeds.generate_sentences
+sentences.each { |sentence| GamesDatum.create!(content: sentence, game_id: game.id) }
