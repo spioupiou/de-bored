5138aab3e8bf435a95b21c0ad9ff08372e5ad781
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-11 15:46:09 +0900

    Devise setup with nickname and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, uniqueness: true
+  before_create :set_nickname
+
+  def email_required?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+
+  private
+
+  def set_nickname
+    self.nickname = self.username
+  end
 end
