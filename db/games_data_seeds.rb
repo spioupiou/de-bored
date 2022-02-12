ce3ac17a6bcdca28a82d5c3938d9667eec6f8e60
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-12 18:46:18 +0900

    Removed neverhaveIever from sentence bit

--- db/games_data_seeds.rb
+++ db/games_data_seeds.rb
@@ -2,14 +2,14 @@ class CreateGamesDataSeeds
 
   def self.generate_sentences
     [
-      "Never have I ever pretended to know someone I didn't.",
-      "Never have I ever watched Star Wars.",
-      "Never have I ever created a Youtube video.",
-      "Never have I ever colored my hair a crazy color.",
-      "Never have I ever caught something on fire while cooking.",
-      "Never have I ever drawn on someone when they were asleep.",
-      "Never have I ever tried parachuting.",
-      "Never have I ever had to wear braces."
+      "pretended to know someone I didn't.",
+      "watched Star Wars.",
+      "created a Youtube video.",
+      "colored my hair a crazy color.",
+      "caught something on fire while cooking.",
+      "drawn on someone when they were asleep.",
+      "tried parachuting.",
+      "had to wear braces."
     ]
   end
 
