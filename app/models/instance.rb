661b024fed84c2e9e322b65714913663763f9270
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-13 10:04:54 +0900

    Created the controllers

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -2,6 +2,4 @@ class Instance < ApplicationRecord
   belongs_to :game
   belongs_to :user # host
   has_many :player_inputs
-
-  validates :status, presence: true, inclusion: { in: %w[pending ongoing completed] }
 end
