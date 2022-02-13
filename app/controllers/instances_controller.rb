b5289703c88fdd4fb81f2f41f173903d8ca3a07c
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-13 10:01:59 +0900

    Created the controllers, model validation

---
+++ app/controllers/instances_controller.rb
@@ -0,0 +1,10 @@
+class InstancesController < ApplicationController
+  def create # Create the game instance
+  end
+
+  def show # Display the game instance with users subscribed (web socket)
+  end
+
+  def update # Update the status, pending -> ongoing -> completed
+  end
+end
--- app/models/instance.rb
+++ app/models/instance.rb
@@ -2,4 +2,6 @@ class Instance < ApplicationRecord
   belongs_to :game
   belongs_to :user # host
   has_many :player_inputs
+
+  validates :status, presence: true, inclusion: { in: %w[pending ongoing completed] }
 end
