f06bdca915d731cf256b0b8599a859afc85c157d
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-13 15:18:49 +0900

    Clicking on game triggers instance creation

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,8 +1,28 @@
 class InstancesController < ApplicationController
   def create # Create the game instance
+    if user_signed_in?
+      # Create an instance with current_user as host
+      @instance = Instance.create!(
+        game_id: params[:game_id],
+        user_id: current_user.id,
+        status: "pending"
+      )
+
+      # Make the host a player
+      Player.create!(
+        user_id: current_user.id,
+        instance_id: @instance.id
+      )
+
+      # Redirect to instance show page
+      redirect_to instance_path(@instance)
+    else
+      redirect_to new_user_session_path # If user not signed in, redirect him
+    end
   end
 
   def show # Display the game instance with users subscribed (web socket)
+    @instance = Instance.find(params[:id])
   end
 
   def update # Update the status, pending -> ongoing -> completed
