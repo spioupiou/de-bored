bbcda10fccc84a3f44379504cd9ae526a0773ab9
Carl Noval <cjrnoval22@gmail.com>
2022-02-15 20:43:18 +0900

    make the fonts we like available in the app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,8 +25,12 @@ require("channels")
 // External imports
 import "bootstrap";
 import {initInstanceChannel} from '../channels/instance_channel'
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import "@fontsource/roboto";
+import "@fontsource/play";
+import "@fontsource/dosis";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
