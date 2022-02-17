9bf15de08904373c198dd340fb9639d0f7e7ff22
Carl Noval <cjrnoval22@gmail.com>
2022-02-17 21:17:51 +0900

    fix wrong reference

--- db/migrate/20220216040304_create_rounds.rb
+++ db/migrate/20220216040304_create_rounds.rb
@@ -2,7 +2,7 @@ class CreateRounds < ActiveRecord::Migration[6.0]
   def change
     create_table :rounds do |t|
       t.integer :number, default: 0, null:false
-      t.references :game_contents, null: false, foreign_key: true
+      t.references :game_content, null: false, foreign_key: true
     end
   end
 end
