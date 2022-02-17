dea467bad1ef19f12908dc14eb469dad9ed9eeca
Carl Noval <cjrnoval22@gmail.com>
2022-02-17 21:12:12 +0900

    fix wrong reference name

--- db/migrate/20220216040631_add_rounds_reference_to_player_inputs.rb
+++ db/migrate/20220216040631_add_rounds_reference_to_player_inputs.rb
@@ -1,5 +1,5 @@
 class AddRoundsReferenceToPlayerInputs < ActiveRecord::Migration[6.0]
   def change
-    add_reference :player_inputs, :rounds, foreign_key: true
+    add_reference :player_inputs, :round, foreign_key: true
   end
 end
