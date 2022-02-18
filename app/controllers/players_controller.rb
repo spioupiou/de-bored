9bdd555b965a6ca9215e315eb461edd3e0b2d01e
Carl Noval <cjrnoval22@gmail.com>
2022-02-18 20:01:29 +0900

    able to broadcast new player joining into the dbi waiting page

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,6 +3,7 @@ class PlayersController < ApplicationController
     redirect_to new_user_session_path unless user_signed_in?
 
     @instance = Instance.find_by_pin(params[:pin])
+
     if @instance.blank?
       redirect_to root_path, notice: "Lobby not found"
     else
@@ -13,9 +14,20 @@ class PlayersController < ApplicationController
           user_id: current_user.id,
           instance_id: @instance.id
         )
+
+        # needed local arguments for the partial below
+        @players = Player.where(instance: @instance)
+        @game = @instance.game
+
+        InstanceChannel.broadcast_to(
+          @instance,
+          render_to_string(
+            partial: "/instances/show_waiting",
+            locals: { players: @players, instance: @instance, game: @game }
+          )
+        )
       end
       redirect_to instance_path(@instance)
     end
-
   end
 end
