6bd86f63318faf2c3cb9209c160e8dc23a127a92
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-18 22:46:04 +0900

    Just for fun

--- db/seeds/game_contents_seeds.rb
+++ db/seeds/game_contents_seeds.rb
@@ -1,14 +1,22 @@
 class CreateGameContentsSeeds
   def self.generate_sentences
     [
-      "pretended to know someone I didn't.",
-      "watched Star Wars.",
-      "created a Youtube video.",
-      "colored my hair a crazy color.",
-      "caught something on fire while cooking.",
-      "drawn on someone when they were asleep.",
-      "tried parachuting.",
-      "had to wear braces."
+      "pushed on master by accident.",
+      "been frustrated with git while working on a project.",
+      "looked for answers on Stack Overflow.",
+      "used Figma for fun.",
+      "broken production.",
+      "adjusted the tests to pass them.",
+      "left an unfinished project on my GitHub.",
+      "exposed my API key unintentionally.",
+      # "pretended to know someone I didn't.",
+      # "watched Star Wars.",
+      # "created a Youtube video.",
+      # "colored my hair a crazy color.",
+      # "caught something on fire while cooking.",
+      # "drawn on someone when they were asleep.",
+      # "tried parachuting.",
+      # "had to wear braces."
     ]
   end
 end
