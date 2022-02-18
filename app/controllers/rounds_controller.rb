4d595960422ed456eb8a25d7036a58f8dce53d42
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-02-18 18:40:56 +0900

    Started to implement the round logic

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,6 +1,21 @@
 class RoundsController < ApplicationController
-  # Start creating a round after Start button has been clicked
+  # Start creating a round after Start button or Next Question button has been clicked
   def create
-    
+    @instance = Instance.find(params[:instance_id])
+    @instance.status = "ongoing"
+
+    @game_content = GameContent.all.sample
+    rounds = Round.where(game_content_id: @game_content.id)
+    @round = Round.create!(
+      number: rounds.count + 1,
+      game_content_id: @game_content.id
+    )
+
+    if @instance.save
+      InstanceChannel.broadcast_to(
+        @instance,
+        render_to_string(partial: "instances/show_question", locals: { game_content: @game_content, round: @round })
+      )
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'games#index' # our landing page with selection of games
   resources :instances, only: [:create, :show, :update] do
-    resources :player_inputs, only: [:create, :index]
+    resources :rounds, only: [:create, :show] do
+      # resources :player_inputs, only: [:create]
+    end
   end
   resources :players, only: [:create]
 end
