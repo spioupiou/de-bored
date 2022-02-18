305db9fed58235bb849de20cc42158824cd0dee5
Carl Noval <cjrnoval22@gmail.com>
2022-02-18 22:31:20 +0900

    remove comments

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -29,26 +29,3 @@ class PlayersController < ApplicationController
     end
   end
 end
-
-
-
-      # # Avoid dupplicate player
-      # player = Player.where(user_id: current_user.id, instance_id: @instance.id)
-      # if player.blank?
-      #   Player.create!(
-      #     user_id: current_user.id,
-      #     instance_id: @instance.id
-      #   )
-
-      #   # needed local arguments for the partial below
-      #   @players = Player.where(instance: @instance)
-      #   @game = @instance.game
-
-      #   InstanceChannel.broadcast_to(
-      #     @instance,
-      #     render_to_string(
-      #       partial: "/instances/show_waiting",
-      #       locals: { players: @players, instance: @instance, game: @game }
-      #     )
-      #   )
-      # end
