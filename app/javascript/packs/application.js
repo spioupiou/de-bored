a8322ab41dfebef217ea2ca222e1911821b92365
Hiroya Takemura <hirotakemura210@gmail.com>
2022-02-20 21:01:42 +0900

    homepage animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,7 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initInstanceChannel } from '../channels/instance_channel'
+import { highlighter } from "./rough_notation";
 // next 3 lines is from https://fontsource.org/fonts
 import "@fontsource/roboto";
 import "@fontsource/play";
@@ -37,4 +38,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initInstanceChannel()
+  highlighter();
 });
