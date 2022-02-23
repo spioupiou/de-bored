27a96bb7648f0f1ee3b021641b7eaf0b0a5c6d49
Carl Noval <cjrnoval22@gmail.com>
2022-02-23 16:19:20 +0900

    added model method for retrieving non-host user

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -9,4 +9,12 @@ class Instance < ApplicationRecord
   validates :max_players, presence:true, numericality: { greater_than_or_equal_to: 2 }
 
   enum status: { waiting: 'waiting', ongoing: 'ongoing', done: 'done' }
+
+  # find user_ids of all players in the instance except for host
+  def non_host_player_user_ids
+    players = Player.where(instance: self).order(id: :desc) # find all players
+    user_ids = players.map(&:user_id)                       # find all player.user_id
+    user_ids.delete(self.user_id)                           # remove host's user_id
+    user_ids                                                # return array of user_id w/o host's user_id
+  end
 end
