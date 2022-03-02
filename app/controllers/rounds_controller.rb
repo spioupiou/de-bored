3522bf49a4fecb403a29c5476db4f985aadbd4f4
Carl Noval <cjrnoval22@gmail.com>
2022-03-03 04:02:50 +0900

    round zero + rounds controller refactor + cleanup on rounds show page + small things here and there

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,13 +3,6 @@ class RoundsController < ApplicationController
 
   # Start creating a round after Start button or Next Question button has been clicked
   def create
-    # protection incase a hidden tag would have a nil value
-    begin
-      current_round_id = params[:current_round].to_i
-      current_round = Round.find(current_round_id)
-    rescue ActiveRecord::RecordNotFound
-    end
-
     @instance = Instance.find(params[:instance_id])
     next_round_number = determine_next_round(@instance)
 
@@ -18,16 +11,15 @@ class RoundsController < ApplicationController
 
       # in memory @round is going to have: number: 0, game_content_id: nil, instance_id: 1, phase: 1
       @round = Round.new(instance: @instance)
-      # to compare who the impostor is on view
       forced_impostor_nickname = "cedrine"
       impostor_player_id = generate_impostor(@instance, forced_impostor_nickname)
       @instance.update(impostor_player_id: impostor_player_id, status: "ongoing")
 
       if @round.save
-        # redirect all subscribers except host to round zero
+        # redirect all subscribers to round zero except host 
         InstanceChannel.broadcast_to(
           @instance,
-          head: 302, # redirection code
+          head: 303,
           path: instance_round_path(@instance, @round)
         )
       end
@@ -35,19 +27,18 @@ class RoundsController < ApplicationController
       # to also redirect host to the next page
       redirect_to instance_round_path(@instance, @round)
     else
-      @round = Round.new(number: next_round_number)
-      @round = generate_unique_question(@instance, @round)
+      # this falsy block means the game already went through round zero
+
+      @round = Round.new(instance: @instance, number: next_round_number)
+      @round = generate_unique_question(@round)
+
+      # get the previous round of the instance, users from that action cable needs to be redirected
+      last_round = Round.where(instance_id: @instance).order(id: :desc).limit(1).first
 
       if @round.save
-        # redirect all subscribers except host to rounds phase1
-        InstanceChannel.broadcast_to(
-          @instance,
-          head: 302, # redirection code
-          path: instance_round_path(@instance, @round)
-        )
         RoundChannel.broadcast_to(
-          current_round,
-          head: 303, # redirection code
+          last_round,
+          head: 303,
           path: instance_round_path(@instance, @round)
         )
       end
@@ -62,9 +53,15 @@ class RoundsController < ApplicationController
     @instance = Instance.find(params[:instance_id])
     @game_id = @instance.game_id
     @round = Round.find(params[:id])
-    @game_content = GameContent.find(@round.game_content_id) if !@round.number.zero?
     @player_inputs = PlayerInput.where(round_id: @round.id)
-    @current_player_is_impostor = (Player.find_by(instance: @instance, user: @current_user).id == @instance.impostor_player_id)
+    
+    if @round.number.zero?
+      # needed to know who the impostor is on round zero
+      @current_player_is_impostor = (Player.find_by(instance: @instance, user: @current_user).id == @instance.impostor_player_id)
+    else
+      # only generate contents to non round zero rounds
+      @game_content = GameContent.find(@round.game_content_id)
+    end
   end
 
   private
@@ -78,8 +75,8 @@ class RoundsController < ApplicationController
     last_round.nil? ? 0 : last_round.number + 1
   end
 
-  def generate_unique_question(instance, round)
-    game = Game.find(instance.game_id)
+  def generate_unique_question(round)
+    game = Game.find(round.instance.game_id)
     game_contents_array = game.game_contents
     game_contents_ids = game_contents_array.map(&:id)
 
@@ -93,16 +90,14 @@ class RoundsController < ApplicationController
       break if round.valid?
     end
 
-    # byebug
     round
   end
 
   def generate_impostor(instance, nickname)
     player_ids = Player.where(instance: instance).pluck(:id)
 
-    # impostor is a random user if force_impostor returns nil
+    # impostor is a random player if force_impostor returns nil
     force_impostor(instance, nickname) || player_ids.sample
-    # force_impostor(users, nickname) || users.pluck(:id).sample
   end
 
   def force_impostor(instance, nickname)
@@ -118,7 +113,7 @@ class RoundsController < ApplicationController
       end
     end
 
-    # if nil then there is no player with given nickname
+    # if nil then there is no player with the given nickname
     impostor
   end
 end
