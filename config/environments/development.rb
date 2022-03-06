549c09febb4c703b50b0d6313f9c4562264c6511
Carl Noval <cjrnoval22@gmail.com>
2022-03-07 03:20:47 +0900

    missed to push on my cloudinary setup pr, I had it on assume-unchanged for some reason

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -60,4 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # to allow requests to e01b-126-74-147-254.ngrok.io
+  config.hosts << "056c-126-74-147-254.ngrok.io"
 end
