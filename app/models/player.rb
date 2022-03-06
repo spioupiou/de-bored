35632f326509271bb68b946e1108a66f7f9cd241
Carl Noval <cjrnoval22@gmail.com>
2022-03-07 03:14:16 +0900

    no need

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,6 @@ class Player < ApplicationRecord
   belongs_to :user
   belongs_to :instance
   has_many :player_inputs, dependent: :destroy
-  has_one_attached :avatar
 
   validates :user_id, uniqueness: { scope: :instance_id }
 end
