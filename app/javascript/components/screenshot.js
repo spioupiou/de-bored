6587a02de6f701b62cf9fedf16ca7b8bdd670b81
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-06 16:33:55 +0900

    Save screenshot

--- app/javascript/components/screenshot.js
+++ app/javascript/components/screenshot.js
@@ -2,8 +2,19 @@ import html2canvas from 'html2canvas';
 
 const captureResults = () => {
   const results = document.querySelector("#result");
-  const shareBtn = document.querySelector("#share-btn");
-  const saveBtn = document.querySelector("#save-logo");
+  const saveBtn = document.querySelector("#download");
+
+  function dataURLtoBlob(dataURL) {
+    // Decode the dataURL
+    var binary = atob(dataURL.split(',')[1]);
+    // Create 8-bit unsigned array
+    var array = [];
+    for (var i = 0; i < binary.length; i++) {
+      array.push(binary.charCodeAt(i));
+    }
+    // Return our Blob object
+    return new Blob([new Uint8Array(array)], { type: 'image/png' });
+  }
 
   const takeScreenshot = () => {
     html2canvas(results, {
@@ -11,24 +22,36 @@ const captureResults = () => {
     }).then(canvas => {
       console.log("Screenshot!")
       const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
-      if (document.getElementById("screenshot")) {
-        document.getElementById("screenshot").src = image;
-      }
+      // Insert image in element
+      // if (document.getElementById("screenshot")) {
+      //   document.getElementById("screenshot").src = image;
+      // }
 
-      if (!!saveBtn) {
-        console.log("Save button identified")
-        saveBtn.addEventListener('click', () => {
-          var link = document.createElement('a');
-          link.download = "myresults_debored.png";
-          link.href = image;
-          link.click();
-        })
-      }
+      // Find a way to save in base64 image
+      var file = dataURLtoBlob(image)
+
+      var fd = new FormData();
+      // Append our Canvas image file to the form data
+      fd.append("image", file);
+      // And send it
+      $.ajax({
+        url: "/screenshot",
+        type: "POST",
+        data: fd,
+        processData: false,
+        contentType: false,
+      });
+
+      // Download the image
+      const link = document.createElement('a');
+      link.download = "myresults_debored.png";
+      link.href = image;
+      link.click();
     });
   }
 
-  if (!!shareBtn) {
-    shareBtn.addEventListener('click', takeScreenshot)
+  if (!!saveBtn) {
+    saveBtn.addEventListener('click', takeScreenshot)
   }
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
     end
     resources :results, only: [:show]
   end
+  post 'screenshot', to: 'screenshots#save'
 end
