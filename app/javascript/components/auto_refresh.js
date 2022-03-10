be90158cac1e74b13a0ff0e292fea9745c95bc2b
Hiroya Takemura <hirotakemura210@gmail.com>
2022-03-11 01:01:06 +0900

    added auto refresh on submitted answers

--- app/javascript/components/auto_refresh.js
+++ app/javascript/components/auto_refresh.js
@@ -1,11 +1,20 @@
 export const updateDiv = () => { 
     const instance = document.getElementById("instance")
     const updateThis = document.getElementById("updateThis")
+    const playerInputs = document.getElementById("player-input-count");
 
     if (!!updateThis){
         setInterval(() => {
             console.log("refreshing..")
             $( "#updateThis" ).load(window.location.href + " #updateThis" );
+    
+        }, 1000)
+    }
+
+    if(!!playerInputs){
+        setInterval(() => {
+            console.log("refreshing..")
+            $( "#player-input-count" ).load(window.location.href + " #player-input-count" );
     
         }, 1000)
     }
