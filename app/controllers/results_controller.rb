3c53f8e6e71ef2ebaffdb09cc3cba7f0db0514c0
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-11 09:34:20 +0900

    Several players and fixed comments

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -28,21 +28,23 @@ class ResultsController < ApplicationController
 
     @announce_winner = determine_winner()
 
-    # Get the total of "Yes" and "No" votes of each player
+    # Get the total of "Yes" and "No" answers of each player
     @total_answers = PlayerInput.where(instance_id: @instance.id).pluck(:player_id, :input_value)
 
-    # Total number of "Yes" votes
+    # Total number of "Yes" answers
     @total_yes = @total_answers.select { |k, v| v == 'Yes' }
     @total_yes_tally = @total_yes.tally
+    @yes_players = []
     @total_yes_tally.each do |id, count|
-      @yes_player = Player.find(id[0]) if count == @total_yes_tally.values.max
+      @yes_players.push(Player.find(id[0])) if count == @total_yes_tally.values.max
     end
 
-    # Total number of "No" votes
+    # Total number of "No" answers
     @total_no = @total_answers.select { |k, v| v == 'No' }
     @total_no_tally = @total_no.tally
+    @no_players = []
     @total_no_tally.each do |id, count|
-      @no_player = Player.find(id[0]) if count == @total_no_tally.values.max
+      @no_players.push(Player.find(id[0])) if count == @total_no_tally.values.max
     end
 
   end
